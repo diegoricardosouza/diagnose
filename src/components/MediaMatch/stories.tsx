889d0c019bdf02eef70ffcfc42b5e1c7aa82f36e
Dieego Ricardo <diegoricardoweb@gmail.com>
2021-09-04 22:08:58 -0300

    Criado componente MediaMatch

---
+++ src/components/MediaMatch/stories.tsx
@@ -0,0 +1,9 @@
+import { Story, Meta } from '@storybook/react'
+import MediaMatch from '.'
+
+export default {
+  title: 'MediaMatch',
+  component: MediaMatch
+} as Meta
+
+export const Default: Story = () => <MediaMatch />
