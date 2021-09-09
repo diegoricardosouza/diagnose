6668a8d9374815c77813a8dc202d38f1d8eed4db
Dieego Ricardo <diegoricardoweb@gmail.com>
2021-09-08 23:30:32 -0300

    Criado handleInput no form

--- src/components/FormBanner/index.tsx
+++ src/components/FormBanner/index.tsx
@@ -1,43 +1,59 @@
+import { useState } from 'react'
+
 import ButtonForm from 'components/ButtonForm'
 import FormHeading from 'components/FormHeading'
 import TextField from 'components/TextField'
 import * as S from './styles'
+import { FieldErrors } from 'utils/validations'
 
-const FormBanner = () => (
-  <S.Wrapper>
-    <S.HeaderWrapper>
-      <FormHeading
-        color="white"
-        title="Quer mais informações?"
-        subtitle="Nossos consultores entram<br> em contato com você."
-      />
-    </S.HeaderWrapper>
+const FormBanner = () => {
+  const [formError, setFormError] = useState('')
+  const [fieldError, setFieldError] = useState<FieldErrors>({})
+  const [values, setValues] = useState({ nome: '', email: '', telefone: '' })
 
-    <form>
-      <TextField
-        name="nome"
-        placeholder="Seu nome:"
-        type="text"
-        color="normal"
-      />
+  const handleInput = (field: string, value: string) => {
+    setValues((s) => ({ ...s, [field]: value }))
+  }
 
-      <TextField
-        name="email"
-        placeholder="Seu melhor e-mail:"
-        type="email"
-        color="normal"
-      />
+  return (
+    <S.Wrapper>
+      <S.HeaderWrapper>
+        <FormHeading
+          color="white"
+          title="Quer mais informações?"
+          subtitle="Nossos consultores entram<br> em contato com você."
+        />
+      </S.HeaderWrapper>
 
-      <TextField
-        name="telefone"
-        placeholder="Telefone com DDD:"
-        type="tel"
-        color="normal"
-      />
+      <form>
+        <TextField
+          name="nome"
+          placeholder="Seu nome:"
+          type="text"
+          color="normal"
+          onInputChange={(v) => handleInput('nome', v)}
+        />
 
-      <ButtonForm type="submit">Enviar</ButtonForm>
-    </form>
-  </S.Wrapper>
-)
+        <TextField
+          name="email"
+          placeholder="Seu melhor e-mail:"
+          type="email"
+          color="normal"
+          onInputChange={(v) => handleInput('email', v)}
+        />
+
+        <TextField
+          name="telefone"
+          placeholder="Telefone com DDD:"
+          type="tel"
+          color="normal"
+          onInputChange={(v) => handleInput('telefone', v)}
+        />
+
+        <ButtonForm type="submit">Enviar</ButtonForm>
+      </form>
+    </S.Wrapper>
+  )
+}
 
 export default FormBanner
