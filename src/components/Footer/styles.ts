9231f5be1e53fa57f8e85c9709438c7b72dda162
Dieego Ricardo <diegoricardoweb@gmail.com>
2025-06-18 23:44:02 -0300

    feat: add icon whatsapp

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -36,3 +36,43 @@ export const SocialWrapper = styled.div`
     `}
   `}
 `
+export const WhatsWrapper = styled.div`
+  position: absolute;
+  right: 15px;
+  bottom: 15px;
+
+  span {
+    position: absolute;
+    right: 115%;
+    top: 5px;
+    border-radius: 5px;
+    text-align: center;
+    font-size: 12px;
+    line-height: 14px;
+    padding: 8px 12px;
+    box-shadow: 0 0 15px rgba(0, 0, 0, 0.15);
+    transition: all 0.3s;
+    min-width: 120px;
+    opacity: 0;
+    visibility: hidden;
+
+    &::before {
+      content: '';
+      width: 16px;
+      height: 16px;
+      transform: rotate(45deg);
+      background: #fff;
+      position: absolute;
+      bottom: 0;
+      top: 0;
+      z-index: 10;
+      right: -4px;
+      margin: auto;
+    }
+  }
+
+  &:hover span {
+    opacity: 1;
+    visibility: visible;
+  }
+`
